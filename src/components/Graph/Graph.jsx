04d08e6a9f33a7fc87a482c6026eeacf60ef6f0e
casder-succ <casderiopus@gmail.com>
2021-11-12 01:03:30 +0300

    feat: make nodes be able to add

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -1,12 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactFlow, { removeElements, addEdge } from "react-flow-renderer";
+
 
 import initialElements from './graphElements';
 import NodeEditor from '../NodeEditor/NodeEditor'
+import {addNode} from '../../utils/index'
 
 import 'react-flow-renderer/dist/style.css';
 import 'react-flow-renderer/dist/theme-default.css';
 import './main.sass'
+
+
 
 const Graph = () => {
   const [elements, setElements] = useState(initialElements);
@@ -15,14 +19,53 @@ const Graph = () => {
   const onElementsRemove = (elementsToRemove) => setElements((els) => removeElements(elementsToRemove, els));
   const onConnect = (params) => setElements((els) => addEdge(params, els));
 
+  // useEffect(() => {
+  //   setElements((els) =>
+  //     els.map((el) => {
+  //       return el;
+  //     })
+  //   );
+  // }, [elements, setElements]);
+  const parseElementContent = (id, content, x, y) => {
+    const a = content.indexOf('[[');
+    const b = content.indexOf(']]', a);
 
+    const label = content.slice(a + 2, b);
+
+    const params1 = {
+      id: `e${id + 1}`,
+      data: {
+        label,
+        content: ''
+      },
+      sourcePosition: 'right',
+      targetPosition: 'left',
+      position: {
+        x: x + 150,
+        y: y + 90
+      }
+    };
+
+    const params2 = {
+      id: `${id}-e${id + 1}`,
+      source: `${id}`,
+      target: `e${id + 1}`
+    };
+
+    if (a >= 0) {
+      setElements((els) => addNode(params1, els));
+      setElements((els) => addEdge(params2, els));
+    }
+  }
 
   const onElementClick = (event, element) => {
     event.preventDefault();
+    if (currElement) {
+      parseElementContent(currElement.id, currElement.data.content, currElement.position.x, currElement.position.y);
+    }
     setCurrElement(element);
+    console.log(elements);
   }
-
-
 
   return (
     <div className='graphField'>
