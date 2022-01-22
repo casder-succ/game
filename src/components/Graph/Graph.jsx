b52b53539318f2a4dbed2036770bb48aaf0a8672
casder <casderiopus@gmail.com>
2022-01-22 18:07:46 +0300

    feat: add useEffect to be able add new nodes, take out base app state

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import ReactFlow, { removeElements, addEdge } from "react-flow-renderer";
+import ReactFlow, {removeElements, addEdge} from "react-flow-renderer";
 
 
 import initialElements from './graphElements';
@@ -11,79 +11,117 @@ import 'react-flow-renderer/dist/theme-default.css';
 import './main.sass'
 
 
-
 const Graph = () => {
-  const [elements, setElements] = useState(initialElements);
-  const [currElement, setCurrElement] = useState(null);
+    const [elements, setElements] = useState(initialElements);
+    const [currElement, setCurrElement] = useState(null);
+    const [elId, setElId] = useState(null);
+    const [nodeName, setNodeName] = useState(null);
+    const [nodeContent, setNodeContent] = useState(null);
 
-  const onElementsRemove = (elementsToRemove) => setElements((els) => removeElements(elementsToRemove, els));
-  const onConnect = (params) => setElements((els) => addEdge(params, els));
+    const onElementsRemove = (elementsToRemove) => setElements((els) => removeElements(elementsToRemove, els));
+    const onConnect = (params) => setElements((els) => addEdge(params, els));
 
-  // useEffect(() => {
-  //   setElements((els) =>
-  //     els.map((el) => {
-  //       return el;
-  //     })
-  //   );
-  // }, [elements, setElements]);
-  const parseElementContent = (id, content, x, y) => {
-    const a = content.indexOf('[[');
-    const b = content.indexOf(']]', a);
-
-    const label = content.slice(a + 2, b);
-
-    const params1 = {
-      id: `e${id + 1}`,
-      data: {
-        label,
-        content: ''
-      },
-      sourcePosition: 'right',
-      targetPosition: 'left',
-      position: {
-        x: x + 150,
-        y: y + 90
-      }
-    };
-
-    const params2 = {
-      id: `${id}-e${id + 1}`,
-      source: `${id}`,
-      target: `e${id + 1}`
-    };
-
-    if (a >= 0) {
-      setElements((els) => addNode(params1, els));
-      setElements((els) => addEdge(params2, els));
+    const onElementEdit = (name, content) => {
+        setElId(currElement.id);
+        setNodeName(name);
+        setNodeContent(content);
+        if (content) parseElementContent(
+            currElement.id,
+            content,
+            currElement.position.x,
+            currElement.position.y
+        );
+        setCurrElement(null);
     }
-  }
 
-  const onElementClick = (event, element) => {
-    event.preventDefault();
-    if (currElement) {
-      parseElementContent(currElement.id, currElement.data.content, currElement.position.x, currElement.position.y);
+    useEffect(() => {
+        setElements((els) =>
+            els.map((el) => {
+                if (el.id === elId) {
+                    el.data = {
+                        ...el.data,
+                        label: nodeName,
+                    };
+                }
+
+                return el;
+            })
+        );
+    }, [elId, nodeName, setElements]);
+
+    useEffect(() => {
+        setElements((els) =>
+            els.map((el) => {
+                if (el.id === elId) {
+                    el.data = {
+                        ...el.data,
+                        content: nodeContent,
+                    };
+                }
+
+                return el;
+            })
+        );
+
+
+    }, [elId, nodeContent, setElements]);
+
+
+    const parseElementContent = (id, content, x, y) => {
+        const a = content.indexOf('[[');
+        const b = content.indexOf(']]', a);
+
+        const label = content.slice(a + 2, b);
+
+        const params1 = {
+            id: `e${id + 1}`,
+            data: {
+                label,
+                content: ''
+            },
+            sourcePosition: 'right',
+            targetPosition: 'left',
+            position: {
+                x: x + 250,
+                y: y + 120
+            }
+        };
+
+        const params2 = {
+            id: `${id}-e${id + 1}`,
+            source: `${id}`,
+            target: `e${id + 1}`
+        };
+
+        if (a >= 0) {
+            setElements((els) => addNode(params1, els));
+            setElements((els) => addEdge(params2, els));
+        }
     }
-    setCurrElement(element);
-    console.log(elements);
-  }
 
-  return (
-    <div className='graphField'>
-      <ReactFlow
-        elements={elements}
-        onElementsRemove={onElementsRemove}
-        onConnect={onConnect}
-        deleteKeyCode={46}
-        nodesDraggable={false}
-        onElementClick={onElementClick}
-        onPaneClick={(evt) => onElementClick(evt, null)}
-      >
+    const onElementClick = (event, element) => {
+        if (currElement) {
+
+        }
+        setCurrElement(element);
+    }
+
+    return (
+        <div className='graphField'>
+            <ReactFlow
+                elements={elements}
+                onElementsRemove={onElementsRemove}
+                onConnect={onConnect}
+                deleteKeyCode={46}
+                nodesDraggable={false}
+                onElementClick={onElementClick}
+            >
 
 
-        {currElement && <NodeEditor node={currElement}/>}
-      </ReactFlow>
-    </div>
-  );
+                {currElement && <NodeEditor node={currElement} onSubmit={onElementEdit}/>}
+            </ReactFlow>
+        </div>
+    );
 };
 
 export default Graph;
