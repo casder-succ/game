6d7a1a9944e77eb8f40b659349706c4bc341ccde
casder <casderiopus@gmail.com>
2022-01-22 16:57:19 +0300

    feat: add submit button

--- src/components/NodeEditor/NodeEditor.jsx
+++ src/components/NodeEditor/NodeEditor.jsx
@@ -1,60 +1,68 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 
 import './main.sass'
 
-const NodeEditor = ({node}) => {
-  const [element, setElement] = useState(node);
-  const [elementName, setELementName] = useState(element.data.label);
-  const [elementContent, setElementContent] = useState(element.data.content);
+const NodeEditor = ({node, onNameChange}) => {
+    const [element, setElement] = useState(node);
+    const [elementName, setElementName] = useState(element.data.label);
+    const [elementContent, setElementContent] = useState(element.data.content);
 
-  useEffect(() => {
-    setElement(node);
-    setELementName(element.data.label);
-    setElementContent(element.data.content);
-  })
+    useEffect(() => {
+        setElement(node);
+        setElementName(element.data.label);
+        setElementContent(element.data.content);
+    }, [node, element.data.label, element.data.content])
 
-  const changeName = (event) => {
-    const temp = element;
-    temp.data.label = event.target.value;
+    const changeName = (event) => {
+        const temp = element;
+        temp.data.label = event.target.value;
 
-    setElement(temp);
-    setELementName(event.target.value)
-  }
+        setElement(temp);
+        setElementName(event.target.value)
+    }
 
-  const changeContent = (event) => {
-    const temp = element;
-    temp.data.content = event.target.value;
+    const changeContent = (event) => {
+        const temp = element;
+        temp.data.content = event.target.value;
 
-    setElement(temp);
-    setElementContent(event.target.value);
-  }
+        setElement(temp);
+        setElementContent(event.target.value);
+    }
 
-  return (
-    <div className='node-editor'>
-      <div className="node-editor__content">
-        <div className="node-editor__item">
-          <label>Label:</label>
-            <input
-              className='input__label'
-              type='text'
-              value={elementName}
-              onChange={(event) => changeName(event)}
-            />
+    const handleSubmit = () => {
+        console.log('lalala');
+    }
 
+    return (
+        <div className='node-editor'>
+            <form onSubmit={handleSubmit} className="node-editor__content">
+                <div className="node-editor__item">
+                    <label>Label:</label>
+                    <input
+                        className='input__label'
+                        type='text'
+                        value={elementName}
+                        onChange={(event) => changeName(event)}
+                    />
+
+                </div>
+
+                <div className="node-editor__item">
+                    <label>Content</label>
+                    <textarea
+                        className='input__content'
+                        value={elementContent}
+                        onChange={(event) => changeContent(event)}
+                    />
+                </div>
+                <input
+                    className="node-editor-submit"
+                    type="submit"
+                    value="Submit"
+                />
+            </form>
         </div>
-
-        <div className="node-editor__item">
-          <label>Content</label>
-          <input
-            className='input__content'
-            type='text'
-            value={elementContent}
-            onChange={(event) => changeContent(event)}
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default NodeEditor;
