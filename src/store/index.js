d1198d1ce16cd8a88b2a3108c93b01476a22ac11
casder <casderiopus@gmail.com>
2022-01-25 19:31:29 +0300

    feat: take out store to an external directory

---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+import {createStore} from "redux";
+import initialElements from './graphElements';
+
+const initialState = {
+    elements: {
+        initialElements
+    }
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "ADD_NODE":
+            break;
+        case "ADD_EDGE":
+            break;
+        case "CHANGE_LABEL":
+            break;
+        case "CHANGE_CONTENT":
+            break;
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+export default store;
