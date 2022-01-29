b9145640c262950518b71798b846bf06e1ab09eb
casder <casderiopus@gmail.com>
2022-01-30 02:08:13 +0300

    feat: fully integrate and fix redux' work to node editor component

--- src/components/NodeEditor/NodeEditor.jsx
+++ src/components/NodeEditor/NodeEditor.jsx
@@ -1,25 +1,36 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
 import './main.sass'
 import {useDispatch, useSelector} from "react-redux";
 
 const NodeEditor = ({node, onSubmit}) => {
     const dispatch = useDispatch();
-    const {label, content} = useSelector(state => state.editorFields.fields);
+    const {id, label, content} = useSelector(state => state.editorFields.fields);
 
-    dispatch({type: "INIT", payload: {label: node.data.label, content: node.data.content}});
+    if (label === "" || (content === "" && node.data.content !== "") || node.id !== id) {
+        console.log( (content === "" && node.data.content !== "") )
+        console.log( (label === "") )
+        console.log( (node.id !== id) )
+        dispatch({type: "INIT", payload: {label: node.data.label, content: node.data.content, id: node.id}});
+    }
 
+
+
+    console.log(node, label, content)
 
     return (
         <div className='node-editor'>
             <div className="node-editor__content">
+                <div className="node-editor__item">
+                    Id: {id}
+                </div>
                 <div className="node-editor__item">
                     <label>Label:</label>
                     <input
                         className='input__label'
                         type='text'
                         value={label}
-                        onChange={(event) => dispatch({type: "CHANGE_LABEL", label: event.target.value})}
+                        onChange={(event) => dispatch({type: "CHANGE", payload: {label: event.target.value, content, id}})}
                     />
 
                 </div>
@@ -29,7 +40,7 @@ const NodeEditor = ({node, onSubmit}) => {
                     <textarea
                         className='input__content'
                         value={content}
-                        onChange={(event) => dispatch({type: "CHANGE_LABEL", content: event.target.value})}
+                        onChange={(event) => dispatch({type: "CHANGE", payload: {content: event.target.value, label, id}})}
                     />
                 </div>
                 <button className="node-editor-submit" onClick={() => onSubmit(label, content)}>Submit</button>
