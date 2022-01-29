49a017e7844952e14e4147e189103a3a0ed2d094
casder <casderiopus@gmail.com>
2022-01-30 00:29:35 +0300

    feat: add new node's action's listeners

--- src/store/elementsReducer.js
+++ src/store/elementsReducer.js
@@ -29,9 +29,25 @@ const elementsReducer = (state = initialState, action) => {
                 elements: state.elements.filter((el) => !ids.includes(el.id))
             };
         case "CHANGE_LABEL":
-            return {};
+            return {
+                ...state,
+                elements: initialState.elements.map((node) => {
+                    if (node.id === action.payload.id) {
+                        node.data.label = action.payload.label;
+                    }
+                    return node;
+                })
+            };
         case "CHANGE_CONTENT":
-            return {};
+            return {
+                ...state,
+                elements: initialState.elements.map((node) => {
+                    if (node.id === action.payload.id) {
+                        node.data.content = action.payload.content;
+                    }
+                    return node;
+                })
+            };
         default:
             return state;
     }
