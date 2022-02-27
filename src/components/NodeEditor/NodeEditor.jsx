738b3103574bcac3f5179974952a7e1cd0633a22
casder <casderiopus@gmail.com>
2022-02-27 22:37:00 +0300

    refactor: update submit method

--- src/components/NodeEditor/NodeEditor.jsx
+++ src/components/NodeEditor/NodeEditor.jsx
@@ -11,32 +11,36 @@ const NodeEditor = ({node, onSubmit}) => {
         dispatch({type: "INIT", payload: {label: node.data.label, content: node.data.content, id: node.id}});
     }
 
+    console .log(node)
+
     return (
-        <div className='node-editor'>
-            <div className="node-editor__content">
-                <div className="node-editor__item">
-                    Id: {id}
-                </div>
+        <div className='editor-wrapper'>
+            <div className="node-editor">
+                <div className="node-editor__content">
+                    <div className="node-editor__item">
+                        Id: {id}
+                    </div>
 
-                <div className="node-editor__item">
-                    <label>Title:</label>
-                    <input
-                        className='input__label'
-                        type='text'
-                        value={label}
-                        onChange={(event) => dispatch({type: "CHANGE", payload: {label: event.target.value, content, id}})}
-                    />
-                </div>
+                    <div className="node-editor__item">
+                        <label>Title:</label>
+                        <input
+                            className='input__label'
+                            type='text'
+                            value={label}
+                            onChange={(event) => dispatch({type: "CHANGE", payload: {label: event.target.value, content, id}})}
+                        />
+                    </div>
 
-                <div className="node-editor__item">
-                    <label>Content</label>
-                    <textarea
-                        className='input__content'
-                        value={content}
-                        onChange={(event) => dispatch({type: "CHANGE", payload: {content: event.target.value, label, id}})}
-                    />
+                    <div className="node-editor__item">
+                        <label>Content</label>
+                        <textarea
+                            className='input__content'
+                            value={content}
+                            onChange={(event) => dispatch({type: "CHANGE", payload: {content: event.target.value, label, id}})}
+                        />
+                    </div>
+                    <button className="node-editor-submit" onClick={() => onSubmit(label, content, node)}>Submit</button>
                 </div>
-                <button className="node-editor-submit" onClick={() => onSubmit(label, content)}>Submit</button>
             </div>
         </div>
     );
