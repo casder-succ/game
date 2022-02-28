88bcfe21a7984baf3fead9f331babe0957c22e38
casder <casderiopus@gmail.com>
2022-02-28 18:18:39 +0300

    refactor: rebase new form component

---
+++ src/components/NodeList/NewNodeForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewNodeForm = () => {
+    return (
+        <div className="create-form">
+            
+        </div>
+    );
+};
+
+export default NewNodeForm;
