8a3a4a02f4a727e83f67a9f34c5973c4b033af43
casder <casderiopus@gmail.com>
2022-02-28 19:41:41 +0300

    feat: add button for adding elements

--- src/components/NodeList/NewNodeForm.jsx
+++ src/components/NodeList/NewNodeForm.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import {useDispatch} from "react-redux";
+import {Node} from '../../utils/nodes';
 
-const NewNodeForm = () => {
+import './main.sass';
+
+const NewNodeForm = ({els}) => {
+    const dispatch = useDispatch();
+
+    const onClick = () => {
+        const position = els.reduce((prev, curr) => {
+            if (!curr.id.startsWith('e')) {
+                if (curr.position.x > prev.x) prev.x = curr.position.x;
+                if (curr.position.y > prev.y) prev.y = curr.position.y;
+            }
+            return prev;
+        }, {x: 0, y: 0})
+        position.x += 150;
+        position.y += 80;
+        const node = new Node(position);
+
+        dispatch({type: "ADD_ELEMENTS", payload: [{...node}]});
+    }
+
     return (
         <div className="create-form">
-            
+            <button onClick={onClick}>Add New</button>
         </div>
     );
 };
