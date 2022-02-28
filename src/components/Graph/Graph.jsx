385aa29010c6045a20048c53e4f367d0e44aacb4
casder <casderiopus@gmail.com>
2022-02-28 19:40:37 +0300

    refactor: separate util functions

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactFlow, {ReactFlowProvider} from "react-flow-renderer";
+import ReactFlow from "react-flow-renderer";
 
 import NodeEditor from '../NodeEditor/NodeEditor'
 import {useDispatch, useSelector} from "react-redux";
@@ -7,12 +7,12 @@ import {useDispatch, useSelector} from "react-redux";
 import 'react-flow-renderer/dist/style.css';
 import 'react-flow-renderer/dist/theme-default.css';
 import './main.sass'
+import {parseElementContent} from "../../utils/parsing";
 
 const Graph = () => {
     const dispatch = useDispatch();
     const elements = useSelector(state => state.elements.elements);
     const currElem = useSelector(state => state.currElement.currElem);
-    const REG_FOR_NODES = /\[[[\sA-Za-z]+]]/gm;
 
     const onElementsRemove = (elementsToRemove) => {
         dispatch({type: "REMOVE_ELEMENTS", payload: elementsToRemove});
@@ -20,7 +20,7 @@ const Graph = () => {
 
     const onConnect = (params) => {
         dispatch({type: "ADD_ELEMENTS", payload: [{...params, id: 'e' + params.source + '-' + params.target}]});
-        dispatch({type: "UPDATE_CONTENT", payload: {id: params.source, content: '\n[[]]'}});
+        dispatch({type: "UPDATE_CONTENT", payload: {id: params.source, content: params.target}});
     }
 
     const onPaneClick = () => {
@@ -41,79 +41,32 @@ const Graph = () => {
 
         if (content && content !== currElem.data.content) {
             dispatch({type: "CHANGE_CONTENT", payload: {id: currElem.id, content}});
-            parseElementContent(
-                currElem.id,
-                content,
-                node,
-                currElem.position.x,
-                currElem.position.y
-            );
+
+            parseElementContent(currElem.id, content, node, currElem.position.x, currElem.position.y)
+                .forEach(el => dispatch(el));
         }
 
         dispatch({type: "UNSET_CURR"});
         document.getElementsByClassName('react-flow__pane')[0].click();
     }
 
-    const parseElementContent = (oldId, content, node, x, y) => {
-        const x_y = [{x: 150, y: 120}, {x: 250, y: 80}, {x: 250, y: -120}, {x: 150, y: -180}]
-        const names_arr = [...content.matchAll(REG_FOR_NODES)];
-
-
-        if (names_arr.length && node.data.links.length < names_arr.length) {
-            names_arr.forEach((el, i) => {
-                
-                const id = elements.length;
-                const label = el[0].slice(2, -2);
-
-                const params1 = {
-                    id: `${id}`,
-                    data: {
-                        label,
-                        content: ''
-                    },
-                    sourcePosition: 'right',
-                    targetPosition: 'left',
-                    position: {
-                        x: x + x_y[node.data.links.length + i].x,
-                        y: y + x_y[node.data.links.length + i].y
-                    }
-                };
-
-                const params2 = {
-                    id: `e${oldId}-${id}`,
-                    source: `${oldId}`,
-                    target: `${id}`
-                };
-
-
-                dispatch({type: "ADD_LINK", payload: {id: oldId, link: id}});
-                dispatch({type: "ADD_ELEMENTS", payload: [params1, params2]});
-            })
-        }
-
-
-
-
-    }
-
-
     return (
         <div className='graphField'>
-                <ReactFlow
-                    elements={elements}
-                    onElementsRemove={onElementsRemove}
-                    onConnect={onConnect}
-                    deleteKeyCode={46}
-                    nodesDraggable={false}
-                    onElementClick={onElementClick}
-                    onPaneClick={onPaneClick}
-                >
+            <ReactFlow
+                elements={elements}
+                onElementsRemove={onElementsRemove}
+                onConnect={onConnect}
+                deleteKeyCode={46}
+                nodesDraggable={false}
+                onElementClick={onElementClick}
+                onPaneClick={onPaneClick}
+            >
 
-                    {currElem && <NodeEditor
-                        node={currElem}
-                        onSubmit={onElementEdit}
-                    />}
-                </ReactFlow>
+                {currElem && <NodeEditor
+                    node={currElem}
+                    onSubmit={onElementEdit}
+                />}
+            </ReactFlow>
         </div>
     );
 
---
+++ src/utils/parsing.js
@@ -0,0 +1,43 @@
+const REG_FOR_NODES = /\[[[\sA-Za-z]+]]/gm;
+
+export const parseElementContent = (oldId, content, node, x, y) => {
+    const x_y = [{x: 180, y: 160}, {x: 250, y: 90}, {x: 240, y: -90}, {x: 150, y: -180}]
+    const names_arr = [...content.matchAll(REG_FOR_NODES)];
+
+    const actions = [];
+
+
+    if (names_arr.length && node.data.links.length < names_arr.length) {
+        names_arr.forEach((el, i) => {
+
+            if (i > node.data.links.length - 1) {
+                const id = `f${(~~(Math.random() * 1e8)).toString(16)}`;
+                const label = el[0].slice(2, -2);
+
+                const params1 = {
+                    id: `${id}`,
+                    data: {
+                        label,
+                        content: ''
+                    },
+                    sourcePosition: 'right',
+                    targetPosition: 'left',
+                    position: {
+                        x: x + x_y[node.data.links.length ? node.data.links.length + i - 1 : 0].x,
+                        y: y + x_y[node.data.links.length ? node.data.links.length + i - 1 : 0].y
+                    }
+                };
+
+                const params2 = {
+                    id: `e${oldId}-${id}`,
+                    source: `${oldId}`,
+                    target: `${id}`
+                };
+
+                actions.push({type: "ADD_LINK", payload: {id: oldId, link: id}},
+                    {type: "ADD_ELEMENTS", payload: [params1, params2]});
+            }
+        })
+    }
+    return actions;
+}
