3ed6170b50f369e8ec231c349db3c4543d4ffde7
casder <casderiopus@gmail.com>
2022-03-11 16:16:27 +0300

    feat: add new form for new fields

--- src/components/NodeEditor/NodeEditor.jsx
+++ src/components/NodeEditor/NodeEditor.jsx
@@ -5,12 +5,21 @@ import {useDispatch, useSelector} from "react-redux";
 
 const NodeEditor = ({node, onSubmit}) => {
     const dispatch = useDispatch();
-    const {id, label, content} = useSelector(state => state.editorFields.fields);
+    const {id, label, content, video, photo} = useSelector(state => state.editorFields.fields);
 
     console.log(node)
 
     if ((label === "" && node.id !== id) || (content === "" && node.id !== id) || node.id !== id) {
-        dispatch({type: "INIT", payload: {label: node.data.label, content: node.data.content, id: node.id}});
+        dispatch({
+            type: "INIT",
+            payload: {
+                label: node.data.label,
+                content: node.data.content,
+                id: node.id,
+                photo: node.data.media.photo,
+                video: node.data.media.video
+            }
+        });
     }
 
     return (
@@ -27,7 +36,10 @@ const NodeEditor = ({node, onSubmit}) => {
                             className='input__label'
                             type='text'
                             value={label}
-                            onChange={(event) => dispatch({type: "CHANGE", payload: {label: event.target.value, content, id}})}
+                            onChange={(event) => dispatch({
+                                type: "CHANGE",
+                                payload: {label: event.target.value, content, id, photo, video}
+                            })}
                         />
                     </div>
 
@@ -36,10 +48,36 @@ const NodeEditor = ({node, onSubmit}) => {
                         <textarea
                             className='input__content'
                             value={content}
-                            onChange={(event) => dispatch({type: "CHANGE", payload: {content: event.target.value, label, id}})}
+                            onChange={(event) => dispatch({
+                                type: "CHANGE",
+                                payload: {content: event.target.value, label, id, photo, video}
+                            })}
                         />
                     </div>
-                    <button className="node-editor-submit" onClick={() => onSubmit(label, content, node)}>Submit</button>
+                    <div className="node-editor__item">
+                        <label>Photo</label>
+                        <textarea
+                            className='input__content'
+                            value={photo}
+                            onChange={(event) => dispatch({
+                                type: "CHANGE",
+                                payload: {content, label, id, photo: event.target.value, video}
+                            })}
+                        />
+                    </div>
+                    <div className="node-editor__item">
+                        <label>Video</label>
+                        <textarea
+                            className='input__content'
+                            value={video}
+                            onChange={(event) => dispatch({
+                                type: "CHANGE",
+                                payload: {content, label, id, photo, video: event.target.value}
+                            })}
+                        />
+                    </div>
+                    <button className="node-editor-submit" onClick={() => onSubmit(label, content, node, photo, video)}>Submit
+                    </button>
                 </div>
             </div>
         </div>
