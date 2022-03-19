9f5757ab1c790a3ef666f03d5bd05ca36142aa70
casder <casderiopus@gmail.com>
2022-03-19 17:53:04 +0300

    feat: rebase setting current element to action creators

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -7,67 +7,30 @@ import {useDispatch, useSelector} from "react-redux";
 import 'react-flow-renderer/dist/style.css';
 import 'react-flow-renderer/dist/theme-default.css';
 import './main.sass'
-import {parseElementContent} from "../../utils/parsing";
+import {onConnect, onElementClick, onElementsRemove, onPaneClick} from "../../utils/graphOnEvent";
 
 const Graph = () => {
     const dispatch = useDispatch();
     const elements = useSelector(state => state.elements.elements);
     const currElem = useSelector(state => state.currElement.currElem);
 
-    const onElementsRemove = (elementsToRemove) => {
-        dispatch({type: "REMOVE_ELEMENTS", payload: elementsToRemove});
-    }
 
-    const onConnect = (params) => {
-        dispatch({type: "ADD_ELEMENTS", payload: [{...params, id: 'e' + params.source + '-' + params.target}]});
-        dispatch({type: "UPDATE_CONTENT", payload: {id: params.source, content: params.target}});
-    }
-
-    const onPaneClick = () => {
-        dispatch({type: "UNSET_CURR"});
-    }
-
-    const onElementClick = (event, element) => {
-        if (!(element.id.startsWith("e"))) {
-            dispatch({type: "SET_CURR", payload: element});
-        }
-    }
-
-    const onElementEdit = (label, content, node, photo, video) => {
-        if (photo !== currElem.data.media.photo || video !== currElem.data.media.video) {
-            dispatch({type: "CHANGE_MEDIA", payload: {id: currElem.id, photo, video}});
-        }
-
-        if (label !== currElem.data.label) {
-            dispatch({type: "CHANGE_LABEL", payload: {id: currElem.id, label}});
-        }
-
-        if (content && content !== currElem.data.content) {
-            dispatch({type: "CHANGE_CONTENT", payload: {id: currElem.id, content}});
-
-            parseElementContent(currElem.id, content, node, currElem.position.x, currElem.position.y)
-                .forEach(el => dispatch(el));
-        }
-
-        dispatch({type: "UNSET_CURR"});
-        document.getElementsByClassName('react-flow__pane')[0].click();
-    }
 
     return (
         <div className='graphField'>
             <ReactFlow
                 elements={elements}
-                onElementsRemove={onElementsRemove}
-                onConnect={onConnect}
+                onElementsRemove={(elements) => onElementsRemove(elements, dispatch)}
+                onConnect={(params) => onConnect(params, dispatch)}
                 deleteKeyCode={46}
                 nodesDraggable={false}
-                onElementClick={onElementClick}
-                onPaneClick={onPaneClick}
+                onElementClick={(event, element) => onElementClick(event, element, dispatch)}
+                onPaneClick={() => onPaneClick(dispatch)}
             >
 
                 {currElem && <NodeEditor
                     node={currElem}
-                    onSubmit={onElementEdit}
+                    onSubmit={(params, node) => onElementEdit(params, node, currElem, dispatch)}
                 />}
             </ReactFlow>
         </div>
