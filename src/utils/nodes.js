71de8774bef58883ab02e93dab43bc97a44b82e5
casder <casderiopus@gmail.com>
2022-03-20 16:46:21 +0300

    fix: fix default id problem

--- src/utils/nodes.js
+++ src/utils/nodes.js
@@ -1,6 +1,6 @@
-const defaultID = `f${(~~(Math.random() * 1e8)).toString(16)}`;
+const getDefaultID = () => `f${(~~(Math.random() * 1e8)).toString(16)}`;
 
-export function NodeFlow(x = 20, y = 20, label, id = defaultID) {
+export function NodeFlow(x = 20, y = 20, label, id = getDefaultID()) {
     this.id = id;
     this.sourcePosition = 'right';
     this.targetPosition = 'left';
