69d5036b8e626d4eeb49397759ad250774b7ee07
casder <casderiopus@gmail.com>
2022-04-17 22:54:24 +0300

    test: fix elements test

--- src/store/elements/elements.test.js
+++ src/store/elements/elements.test.js
@@ -25,7 +25,7 @@ describe("elements reducer", function () {
         const edge = {id: "ef1-f3", source: 'f1', target: 'f3'}
         const newState = elementsReducer(state, {type: EDGES__ADD_EDGE, payload: edge})
 
-        expect(newState).toEqual({...state, edges: [...state.edges, edge]})
+        expect(newState).toEqual({...state, edges: [...state.edges]})
     });
 
 });
