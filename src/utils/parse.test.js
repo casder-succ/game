16b66ba57e1a022d916dcf21f5d17e0a68b843e4
casder <casderiopus@gmail.com>
2022-04-17 22:54:59 +0300

    test: create tests for parsing one new node suite

---
+++ src/utils/parse.test.js
@@ -0,0 +1,49 @@
+import elements from "../store/state/graphElements";
+import {parseElementContent} from "./parsing";
+import elementsReducer from "../store/elements/elementsReducer";
+
+const state = {
+    nodes: [...elements.nodes],
+    edges: [...elements.edges],
+};
+
+describe("parsing content with one new node", function () {
+    const element = state.nodes[0];
+    let actions;
+    let newState = {...state};
+    let newNode;
+    let newEdge;
+    beforeEach(() => {
+        actions = parseElementContent(element.data.content + "[[anna]]", element, element.position.x, element.position.y);
+        for (const action of actions) {
+            newState = elementsReducer(newState, action);
+        }
+        newNode = newState.nodes.find(node => !elements.nodes.find(n => n.id === node.id));
+        newEdge = newState.edges.find((edge => !elements.edges.find((e => e.id === edge.id))))
+    })
+
+    it('should create new element', function () {
+        expect(newState.nodes.length).toBe(4);
+    });
+
+    it('should create new element with given label', function () {
+        expect(newNode.data.label).toBe("anna");
+    });
+
+    it('should create link on new node', function () {
+        expect(newState.nodes.find(node => node.id === element.id).data.links).toEqual([{id: 'f2', label: 'Emma sends letter', position: "end"}, {label: "anna", id: newNode.id}]);
+    });
+
+    it('should create edge', function () {
+        expect(newState.edges.length).toBe(elements.edges.length + 1);
+    });
+
+    it('should create edge from given node', function () {
+        expect(newEdge.source).toBe(element.id);
+    });
+
+    it('should create edge to new node', function () {
+        expect(newEdge.target).toBe(newNode.id);
+    });
+});
+
